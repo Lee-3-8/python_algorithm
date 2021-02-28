412a4bb712f6fd936a252a184a9e3c727f8b64cc
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-03-01 01:39:23 +0900

    [Class 2] 요세푸스

--- 백준/Class2/요세푸스.py
+++ 백준/Class2/요세푸스.py
@@ -1,6 +1,20 @@
-n,m = map(int,input().split())
-arr = [i for i in range(1,n+1)]
-print(arr[1:9])
-# i = m
-# while arr:
-#     arr.pop(i)
+
+def main():
+    n,m = map(int,input().split())
+    arr = [i for i in range(1,n+1)]
+    result = []
+    index = m -1
+
+    while True:
+        result.append(arr.pop(index))
+        if not arr: break;
+        index = (index + m - 1) % (len(arr))
+
+    print(f'<{result[0]}',end='')
+    for i in result[1: ]:
+        print(f', {i}',end='')
+    print(">")
+    return
+    
+if __name__=="__main__":
+    main()
