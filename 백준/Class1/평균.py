f47f0d1e6cdc429748687446c87f143fbc677dbd
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-05-01 23:59:00 +0900

    Revert "[Class 1]평균"
    This reverts commit 54b4143e1ebc414a2632b677d496bca1d269fd41.

--- 백준/Class1/평균.py
+++
@@ -1,9 +0,0 @@
-
-def solution(n,arr):
-    m = max(arr)
-    return sum(map(lambda x: x/m*100,arr)) /len(arr)
-
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int,input().split()))
-    print(solution(n,arr))
