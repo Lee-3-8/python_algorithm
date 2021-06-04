07965af7bd63fa24f3708e90f71cb02f6231ad6f
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-06-04 23:58:53 +0900

    Create 계단_오르기.py

---
+++ 백준/Class3/계단_오르기.py
@@ -0,0 +1,14 @@
+
+
+# 재도전 할것 
+
+def solution(n,arr):
+    for i in range(n):
+        arr[i]
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = []
+    for i in range(n):
+        arr.append(int(input()))
+    print(solution(n,arr))
