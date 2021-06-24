7ee1bd8428c93552a6546c38bf5585d913a36518
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-06-24 23:36:33 +0900

    [class 3]연결 요소의 개수
     bfs로 푸니까 시간초과가 나고
    dfs로 푸니까 시간초과가 나지 않았다..

---
+++ 백준/Class3/연결_요소의_개수.py
@@ -0,0 +1,40 @@
+from collections import defaultdict,deque
+import sys
+sys.setrecursionlimit(15000)
+input = sys.stdin.readline
+
+def init_graph(m):
+    dic = defaultdict(list)
+    for i in range(m):
+        start,end = map(int,input().split())
+        dic[start].append(end)
+        dic[end].append(start)
+    return dic
+
+def solution(n,dic):
+    result = 0
+    visited = set()
+
+    def dfs(node):
+        visited.add(node)
+        for i in dic[node]:
+            if i not in visited:
+                dfs(i)
+
+    def bfs(node):
+        que = deque([node])
+        while que:
+            visited.add(que[0])
+            for j in dic[que.popleft()]:
+                if j not in visited:
+                    que.append(j)
+    for i in range(1,n+1):
+        if i not in visited:
+            dfs(i)
+            result += 1
+    return result
+
+if __name__ =='__main__':
+    n,m = map(int, input().split())
+    dic = init_graph(m)
+    print(solution(n, dic))
