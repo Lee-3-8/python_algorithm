d28ae80e096179d690f29330ebe4697b9e606dee
lee-3-8 <leesinji8@gmail.com>
2022-01-06 01:43:17 +0900

    [Class 3] 계단_오르기

--- 백준/Class3/계단_오르기.py
+++ 백준/Class3/계단_오르기.py
@@ -1,14 +1,21 @@
 
-
-# 재도전 할것 
-
-def solution(n,arr):
+def solution(n, arr):
+    dp = [0]*n
     for i in range(n):
-        arr[i]
+        if i == 0:
+            dp[0] = arr[0]
+        elif i == 1:
+            dp[1] = arr[0] + arr[1]
+        elif i == 2:
+            dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+        else:
+            dp[i] = max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i]
+    return dp[n-1]
+
 
 if __name__ == '__main__':
     n = int(input())
     arr = []
     for i in range(n):
         arr.append(int(input()))
-    print(solution(n,arr))
+    print(solution(n, arr))
